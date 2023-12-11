43159b4073df23c6734a622c284a08f1e19c8d3a
deependra <deependrachouhan82@gmail.com>
2023-12-11 04:02:44 -0800

    parsially completed inheritance mapping

--- src/main/java/io/health/repository/ReportRespo.java
+++ src/main/java/io/health/repository/ReportRespo.java
@@ -1,12 +1,17 @@
 package io.health.repository;
 
-import io.health.entities.CBCReport;
 import io.health.entities.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReportRespo extends JpaRepository<Report,Integer> {
+
 
 
 }
